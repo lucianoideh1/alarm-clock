94cbf005adaba97417da8feb9422696583fc5576
Luciano Vazquez <vazquezluciano5@gmail.com>
2022-10-07 13:13:54 -0300

    now the options in hours are written in javascript

---
+++ script.js
@@ -0,0 +1,7 @@
+const selectMenu = document.querySelectorAll("select");
+
+for(let i = 12; i > 0; i--){
+    i = i < 10 ? "0" + i : i;
+    let option = `<option value = "${i}">${i}</option>`;
+    selectMenu[0].firstElementChild.insertAdjacentHTML("afterend",option)
+}
